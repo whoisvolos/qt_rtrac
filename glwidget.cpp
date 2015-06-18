b1113a52723e0d4702e47eca51e6de82e1f46d33
Sergey Golubev <sgolubev@ozon.ru>
2015-06-18 13:01:47 +0300

    Disable cullface and make shaders 2-sided

--- glwidget.cpp
+++ glwidget.cpp
@@ -149,7 +149,7 @@ void GLWidget::paintGL() {
     if (m_model) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glEnable(GL_DEPTH_TEST);
-        glEnable(GL_CULL_FACE);
+        //glEnable(GL_CULL_FACE);
 
         m_world.setToIdentity();
         m_world.rotate(m_xRot / 16.0f, 1, 0, 0);
--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -12,7 +12,27 @@ void main() {
     // Reflection using normal
     vec3 r = reflect(-s, normal);
     // Diffuse intensity
-    vec3 di = vec3(max(dot(s, normal), 0.0));
+    vec3 di = vec3(abs(dot(s, normal)));
+    // Specilar intensity
+    vec3 si = vec3(0.0);
+    if (abs(dot(s, normal)) > 0.0) {
+        si = vec3(pow(abs(dot(r, v)), 256.0));
+    }
+    // Final intensity
+    gl_FragColor = vec4(clamp(0.9 * di + 1.0 * si, 0.0, 1.0), 1.0);
+}
+/*
+void main() {
+    // Normalize normal :)
+    vec3 normal = normalize(vertNormal);
+    // Light dir
+    vec3 s = normalize(lightPos - vert);
+    // View dir (camera @ (0,0,0))
+    vec3 v = normalize(-vert);
+    // Reflection using normal
+    vec3 r = reflect(-s, normal);
+    // Diffuse intensity
+    vec3 di = vec3(abs(dot(s, normal)));
     // Specilar intensity
     vec3 si = vec3(0.0);
     if (dot(s, normal) > 0.0) {
@@ -21,3 +41,4 @@ void main() {
     // Final intensity
     gl_FragColor = vec4(clamp(0.9 * di + 1.0 * si, 0.0, 1.0), 1.0);
 }
+*/
